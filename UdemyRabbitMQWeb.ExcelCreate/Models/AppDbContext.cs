720154660bf782269911697d63c1bd788672ff76
Your Name <you@example.com>
2025-09-29 10:36:41 +0300

    Add project files.

---
+++ UdemyRabbitMQWeb.ExcelCreate/Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace UdemyRabbitMQWeb.ExcelCreate.Models
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        
+        public DbSet<UserFile> UserFiles { get; set; }
+
+    }
+}
